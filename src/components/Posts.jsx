e5f511725111b423b46e24e0a85708ff717a46b3
Tom Blacknell <tom.websites@fastmail.co.uk>
2020-05-23 17:38:32 +0100

    responsive styling

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -27,23 +27,25 @@ const Posts = () => {
   return posts.map(({ node }) => {
     const title = node.frontmatter.title || node.fields.slug;
     return (
-      <article key={node.fields.slug}>
-        <header>
-          <h3>
-            <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-              {title}
-            </Link>
-          </h3>
-          <small>{node.frontmatter.date}</small>
-        </header>
-        <section>
-          <p
-            dangerouslySetInnerHTML={{
-              __html: node.frontmatter.description || node.excerpt,
-            }}
-          />
-        </section>
-      </article>
+      <div className="container">
+        <article key={node.fields.slug}>
+          <header>
+            <h3>
+              <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                {title}
+              </Link>
+            </h3>
+            <small>{node.frontmatter.date}</small>
+          </header>
+          <section>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: node.frontmatter.description || node.excerpt,
+              }}
+            />
+          </section>
+        </article>
+      </div>
     );
   });
 };
