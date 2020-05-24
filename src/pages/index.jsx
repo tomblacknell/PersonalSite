f80fc9e47180cef4ea3e168288496200fa2fb426
Tom Blacknell <tom.websites@fastmail.co.uk>
2020-05-24 13:06:02 +0100

    fix width

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,11 +16,12 @@ const Index = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="Home" />
       <Bio />
-      <h3 className="section-heading">Posts</h3>
+      <h3 className="section-head">Posts</h3>
+      <p className="section-subhead">Look at all these interesting blog posts</p>
       <Posts />
-      <h3 className="section-heading">Projects</h3>
+      <h3 className="section-head">Projects</h3>
+      <p className="section-subhead">Read about and play with some of my side projects</p>
       <Projects />
-      <div style={{ marginTop: '700px' }} />
     </Layout>
   );
 };
