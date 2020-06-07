72ef08cf19da2139d47e1632180d1deb774c4990
Tom Blacknell <tom.websites@fastmail.co.uk>
2020-06-07 16:38:54 +0100

    setup analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        // trackingId: 'ADD YOUR TRACKING ID HERE',
+        trackingId: 'UA-78616781-2',
       },
     },
     'gatsby-plugin-feed',
