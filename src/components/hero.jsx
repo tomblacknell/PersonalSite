13cb787d8c51906496fe0b28ce4f2147ee029f81
Tom Blacknell <tom.websites@fastmail.co.uk>
2020-07-09 14:44:18 +0100

    replace hero image with canvas

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,60 +1,112 @@
-import React from 'react';
-import BackgroundImage from 'gatsby-background-image';
-import { useStaticQuery, graphql } from 'gatsby';
+/* eslint-disable no-undef */
+import React, { useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
 
 import scrollTo from '../utils/scrollTo';
 
+const colors = [
+  ['rgba(248, 255, 233, 1)', 'rgba(237, 173, 58, 0.4)', 'rgba(80, 44, 17, 0.4)'], // yellow
+  ['rgba(247, 216, 221, 1)', 'rgba(210, 100, 64, 0.4)', 'rgba(60, 25, 30, 0.4)'], // red
+  ['rgba(180, 192, 224, 1)', 'rgba(115, 102, 165, 0.4)', 'rgba(32, 31, 58, 0.4)'], // purple
+];
+
 const Hero = ({ children }) => {
-  const { hero4K } = useStaticQuery(graphql`
-  query HeroQuery {
-    hero4K: file(absolutePath: { regex: "/hero-4k.jpg/" }) {
-      childImageSharp {
-        fluid(quality: 100 maxWidth: 5300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
+  const background = useRef(null);
+  const canvas = useRef(null);
+
+  function fixDpi(dpi) {
+    const styleHeight = window.getComputedStyle(canvas.current).getPropertyValue('height').slice(0, -2);
+    const styleWidth = window.getComputedStyle(canvas.current).getPropertyValue('width').slice(0, -2);
+    canvas.current.setAttribute('height', styleHeight * dpi);
+    canvas.current.setAttribute('width', styleWidth * dpi);
   }
-`);
+
+  function draw() {
+    const ctx = canvas.current.getContext('2d');
+    const dpi = window.devicePixelRatio;
+    fixDpi(dpi);
+    ctx.beginPath();
+    ctx.fillStyle = 'black';
+    ctx.rect(0, 0, background.current.width, background.current.height);
+    ctx.fill();
+    for (let n = 0; n < 100; n += 1) {
+      const [innerCol, outerCol, glowCol] = colors[Math.floor(Math.random() * colors.length, 10)];
+      const x = parseInt(Math.random() * canvas.current.width, 10);
+      const y = parseInt(Math.random() * canvas.current.height, 10);
+      const radius = Math.random() * 5;
+      // glow
+      ctx.beginPath();
+      ctx.arc(x, y, radius * 4, 0, Math.PI * 2, false);
+      ctx.fillStyle = glowCol;
+      ctx.fill();
+      ctx.closePath();
+      // outer
+      ctx.beginPath();
+      ctx.arc(x, y, radius * 2, 0, Math.PI * 2, false);
+      ctx.fillStyle = outerCol;
+      ctx.fill();
+      ctx.closePath();
+      // inner
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+      ctx.fillStyle = innerCol;
+      ctx.fill();
+      ctx.closePath();
+    }
+    // requestAnimationFrame(draw);
+  }
+
+  useEffect(() => {
+    requestAnimationFrame(draw);
+  }, []);
 
   return (
-    <div style={{ backgroundColor: 'black' }}>
-      <BackgroundImage
-        Tag="section"
-        className="hero"
-        fluid={hero4K.childImageSharp.fluid}
-      >
-        <div style={{ height: '100%' }}>
-          {children}
-        </div>
-        <div className="row justify-center">
-          <FontAwesomeIcon onClick={() => scrollTo('projects')} className="icon-hero bounce" size="2x" icon={faAngleDoubleDown} />
-          <small
+    <div className="canvas-hero">
+      <canvas
+        style={{ backgroundColor: 'black' }}
+        id="background-canvas"
+        ref={background}
+        className="canvas"
+      />
+      <canvas
+        id="stars-canvas"
+        ref={canvas}
+        className="canvas"
+      />
+      <div style={{ height: '100%' }}>
+        {children}
+      </div>
+      <div className="row justify-center">
+        <FontAwesomeIcon
+          onClick={() => scrollTo('projects')}
+          className="icon-hero bounce"
+          size="2x"
+          icon={faAngleDoubleDown}
+        />
+        <small
+          style={{
+            color: 'white',
+            bottom: 0,
+            position: 'absolute',
+            marginBottom: 10,
+            right: 10,
+          }}
+        >
+          <a
+            href="https://github.com/tomblacknell/PersonalSite/blob/master/src/components/hero.jsx"
+            target="_blank"
+            rel="noreferrer"
             style={{
-              color: 'white',
-              bottom: 0,
-              position: 'absolute',
-              marginBottom: 10,
-              right: 10,
+              color: 'rgb(135, 135, 135)',
+              textDecoration: 'underline',
+              cursor: 'pointer',
             }}
           >
-            <a
-              href="https://www.nasa.gov/mission_pages/hubble/multimedia/index.html"
-              target="_blank"
-              rel="noreferrer"
-              style={{
-                color: 'rgb(135, 135, 135)',
-                textDecoration: 'underline',
-                cursor: 'pointer',
-              }}
-            >
-              Source
-            </a>
-          </small>
-        </div>
-      </BackgroundImage>
+            Source
+          </a>
+        </small>
+      </div>
     </div>
   );
 };
