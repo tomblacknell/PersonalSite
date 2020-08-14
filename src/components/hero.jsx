d288951576b701f634f75585a7f2663f9646fdd4
Tom Blacknell <tom.websites@fastmail.co.uk>
2020-08-14 20:47:14 +0100

    made stars fade-in

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -52,8 +52,12 @@ const Hero = ({ children }) => {
 
   useEffect(() => {
     let frame;
+    const ctx = canvas.current.getContext('2d');
+    ctx.globalAlpha = 0.0;
     const animate = () => {
-      const ctx = canvas.current.getContext('2d');
+      if (frame < 200) {
+        ctx.globalAlpha += 0.005;
+      }
       ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
       ctx.beginPath();
       ctx.fillStyle = 'black';
